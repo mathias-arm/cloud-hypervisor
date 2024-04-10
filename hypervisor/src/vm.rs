858473fe2b3821b14b821d9592f56f54d01668f2
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-04-10 18:12:07 +0100

    hypervisor: kvm: Add Guest memfd functions
    KVM now adds the ability to manage private guest memory through guest
    memfd.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- hypervisor/src/vm.rs
+++ hypervisor/src/vm.rs
@@ -32,6 +32,17 @@ use crate::cpu::Vcpu;
 #[cfg(target_arch = "x86_64")]
 use crate::ClockData;
 use crate::{IoEventAddress, IrqRoutingEntry, UserMemoryRegion};
+#[cfg(feature = "sev_snp")]
+use igvm_defs::IGVM_VHS_SNP_ID_BLOCK;
+use std::any::Any;
+#[cfg(target_arch = "x86_64")]
+use std::fs::File;
+use std::os::unix::io::RawFd;
+use std::sync::Arc;
+#[cfg(target_arch = "aarch64")]
+use std::sync::Mutex;
+use thiserror::Error;
+use vmm_sys_util::eventfd::EventFd;
 
 ///
 /// I/O events data matches (32 or 64 bits).
@@ -279,6 +290,11 @@ pub enum HypervisorVmError {
     #[cfg(feature = "sev_snp")]
     #[error("Failed to mmap:")]
     MmapToRoot,
+    ///
+    /// Failed to create a guest memfd
+    ///
+    #[error("Failed to create guest memfd: {0}")]
+    CreateGuestMemfd(#[source] anyhow::Error),
 }
 ///
 /// Result type for returning from a function
@@ -478,6 +494,11 @@ pub trait Vm: Send + Sync + Any {
     #[cfg(feature = "sev_snp")]
     fn gain_page_access(&self, _gpa: u64, _size: u32) -> Result<()> {
         Ok(())
+    }
+
+    /// Create a guest memfd
+    fn create_guest_memfd(&self, _size: u64) -> Result<RawFd> {
+        unimplemented!()
     }
 }
 

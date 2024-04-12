1f3b857c2517a305200c3236810c274243f43e97
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-04-12 09:32:41 +0100

    vmm: Force DMA accesses with arm_rme
    Enable "force_iommu" when arm_rme is enabled. It's a misnomer because
    there isn't necessarily an IOMMU involved, it just tells the guest to
    perform DMA on virtio as on a normal device - explicitly share memory
    with the device so that the host is able to access it. It's needed for
    arm_rme because by default all memory is private to the guest.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -542,16 +542,20 @@ impl Vm {
         let numa_nodes =
             Self::create_numa_nodes(config.lock().unwrap().numa.clone(), &memory_manager)?;
 
-        #[cfg(feature = "tdx")]
-        let tdx_enabled = config.lock().unwrap().is_tdx_enabled();
-        #[cfg(feature = "sev_snp")]
-        let sev_snp_enabled = config.lock().unwrap().is_sev_snp_enabled();
-        #[cfg(feature = "tdx")]
-        let force_iommu = tdx_enabled;
-        #[cfg(feature = "sev_snp")]
-        let force_iommu = sev_snp_enabled;
-        #[cfg(not(any(feature = "tdx", feature = "sev_snp")))]
-        let force_iommu = false;
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "tdx")] {
+                let tdx_enabled = config.lock().unwrap().is_tdx_enabled();
+                let force_iommu = tdx_enabled;
+            } else if #[cfg(feature = "sev_snp")] {
+                let sev_snp_enabled = config.lock().unwrap().is_sev_snp_enabled();
+                let force_iommu = sev_snp_enabled;
+            } else if #[cfg(feature = "arm_rme")] {
+                let arm_rme_enabled = config.lock().unwrap().is_arm_rme_enabled();
+                let force_iommu = arm_rme_enabled;
+            } else {
+                let force_iommu = false;
+            }
+        }
 
         #[cfg(feature = "guest_debug")]
         let stop_on_boot = config.lock().unwrap().gdb;

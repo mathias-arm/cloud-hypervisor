848a86d868cd41cde53ea3ef23a7973f6eaca03b
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-04-12 09:30:51 +0100

    vmm: Don't log dirty pages on arm_rme
    Logging dirty pages isn't supported at the moment
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- vmm/src/memory_manager.rs
+++ vmm/src/memory_manager.rs
@@ -1207,10 +1207,12 @@ impl MemoryManager {
             .ok_or(Error::CreateSystemAllocator)?,
         ));
 
-        #[cfg(not(feature = "tdx"))]
+        #[cfg(not(any(feature = "tdx", feature = "arm_rme")))]
         let dynamic = true;
         #[cfg(feature = "tdx")]
         let dynamic = !tdx_enabled;
+        #[cfg(feature = "arm_rme")]
+        let dynamic = !arm_rme_enabled;
 
         let acpi_address = if dynamic
             && config.hotplug_method == HotplugMethod::Acpi
@@ -1262,7 +1264,7 @@ impl MemoryManager {
             guest_ram_mappings: Vec::new(),
             boot_data: BTreeMap::new(),
             acpi_address,
-            log_dirty: dynamic, // Cannot log dirty pages on a TD
+            log_dirty: dynamic, // Cannot log dirty pages on a TD or Realm
             arch_mem_regions,
             ram_allocator,
             dynamic,

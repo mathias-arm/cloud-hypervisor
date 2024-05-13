7f4ab657c3dba54e3aa95d8522016c2dcc0a5fd9
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-05-13 21:25:12 +0100

    aarch64: Make PSCI method configurable
    Arm CCA uses the SMC method.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- arch/src/aarch64/mod.rs
+++ arch/src/aarch64/mod.rs
@@ -25,6 +25,11 @@ use crate::{DeviceType, GuestMemoryMmap, NumaNodes, PciSpaceInfo, RegionType};
 
 pub const _NSIG: i32 = 65;
 
+pub enum PsciMethod {
+    Smc,
+    Hvc,
+}
+
 /// Errors thrown while configuring aarch64 system.
 #[derive(Debug, Error)]
 pub enum Error {
@@ -144,6 +149,7 @@ pub fn configure_system<T: DeviceInfoForFdt + Clone + Debug, S: ::std::hash::Bui
     gic_device: &Arc<Mutex<dyn Vgic>>,
     numa_nodes: &NumaNodes,
     pmu_supported: bool,
+    psci_method: PsciMethod,
 ) -> super::Result<usize> {
     let fdt_final = fdt::create_fdt(
         guest_mem,
@@ -157,6 +163,7 @@ pub fn configure_system<T: DeviceInfoForFdt + Clone + Debug, S: ::std::hash::Bui
         numa_nodes,
         virtio_iommu_bdf,
         pmu_supported,
+        psci_method,
     )
     .map_err(|_| Error::SetupFdt)?;
 

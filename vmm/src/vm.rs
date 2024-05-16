23bcd1696eb95ed07218e2d1efe3f756fcf4528f
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-05-16 10:22:02 +0100

    vmm: Handle memory fault exit
    When using guest memfd, KVM may return with a memory fault exit reason,
    indicating that some pages need to be switched from private to shared or
    vice versa. Call the memory manager to do remove the range from either
    the host view of guest memory (shared->private), or from the guest memfd
    (private->shared).
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -394,6 +394,7 @@ impl VmState {
 }
 
 struct VmOpsHandler {
+    memory_manager: Arc<Mutex<MemoryManager>>,
     memory: GuestMemoryAtomic<GuestMemoryMmap>,
     #[cfg(target_arch = "x86_64")]
     io_bus: Arc<Bus>,
@@ -467,7 +468,10 @@ impl VmOps for VmOpsHandler {
         gpa: u64,
         size: u64,
     ) -> result::Result<(), HypervisorVmError> {
-        todo!();
+        self.memory_manager
+            .lock()
+            .unwrap()
+            .memory_fault(fault_type, gpa, size)
     }
 }
 
@@ -565,6 +569,7 @@ impl Vm {
         let mmio_bus = Arc::new(Bus::new());
 
         let vm_ops: Arc<dyn VmOps> = Arc::new(VmOpsHandler {
+            memory_manager: memory_manager.clone(),
             memory,
             #[cfg(target_arch = "x86_64")]
             io_bus: io_bus.clone(),

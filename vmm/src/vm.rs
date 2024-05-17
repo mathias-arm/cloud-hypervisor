1070214cf446ce8c1e4a312115217a6c1ca6385c
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-05-17 13:44:02 +0100

    vmm: RME: Finalize Realm
    Issue the hypervisor call to finalize the RECs and Realm.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -303,6 +303,10 @@ pub enum Error {
     #[error("Error populating Realm VM: {0}")]
     PopulateArmRme(#[source] hypervisor::HypervisorVmError),
 
+    #[cfg(feature = "arm_rme")]
+    #[error("Error finalizing Realm VM: {0}")]
+    FinalizeArmRme(#[source] hypervisor::HypervisorVmError),
+
     #[cfg(feature = "guest_debug")]
     #[error("Error debugging VM: {0:?}")]
     Debug(DebuggableError),
@@ -2256,6 +2260,16 @@ impl Vm {
                     .arm_rme_realm_populate(addr.0, data.size as u64, data.populate)
                     .map_err(Error::PopulateArmRme)?;
             }
+
+            self.cpu_manager
+                .lock()
+                .unwrap()
+                .rec_finalize()
+                .map_err(Error::CpuManager)?;
+
+            self.vm
+                .arm_rme_realm_finalize()
+                .map_err(Error::FinalizeArmRme)?;
         }
 
         self.cpu_manager

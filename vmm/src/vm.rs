4d6ef423c472305e6c09055d52979bb9d64c0c22
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-05-22 18:14:34 +0100

    vmm: RME: Create Realm Descriptor
    Call the hypervisor to create the Realm Descriptor
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -293,6 +293,10 @@ pub enum Error {
     #[error("Invalid TDX payload type")]
     InvalidPayloadType,
 
+    #[cfg(feature = "arm_rme")]
+    #[error("Error creating Realm VM: {0}")]
+    CreateArmRme(#[source] hypervisor::HypervisorVmError),
+
     #[cfg(feature = "guest_debug")]
     #[error("Error debugging VM: {0:?}")]
     Debug(DebuggableError),
@@ -2214,7 +2218,9 @@ impl Vm {
 
         #[cfg(feature = "arm_rme")]
         if self.config.lock().unwrap().is_arm_rme_enabled() {
-            todo!("finalize realm");
+            self.vm
+                .arm_rme_realm_create()
+                .map_err(Error::CreateArmRme)?;
         }
 
         self.cpu_manager
